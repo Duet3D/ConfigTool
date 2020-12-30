ee26f49e22da38da3d734c433cba910cb3c77d66
Jay <github@jayuk.org>
2020-12-30 11:19:41 +0000

    added portuguese, removed other languages until they are ready

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -2,8 +2,7 @@
   <div class="locale-switcher">
     <select v-model="$i18n.locale">
       <option value="en">English</option>
-      <option value="de">Deutsche</option>
-	  <option value="es">Español</option>
+      <option value="pt">Português</option>
     </select>
   </div>
 </template>
